429b42a8727c5b46a925685f9d7e4164e4fab19f
Juan Luis Magaña Paz <opomola@gmail.com>
2021-12-05 20:09:43 -0600

    Renaming of modules because had conflicts with another ones with same name

---
+++ Sistema_Mem_Magana_Juan/src/addrDecoderRAM1.v
@@ -0,0 +1,19 @@
+module addrDecoderRAM1 #(
+parameter WIDTH = 32,
+parameter numPosMem = 64
+)(
+input [WIDTH-1:0] addr,
+output reg [$clog2(numPosMem)-2:0] outAddr
+);
+
+always @(addr) begin
+	if(addr>=32'h10010000 && addr<32'h100100ff) 
+		outAddr = (addr-32'h10010000)>>2;
+	else 
+		outAddr = 5'bx;
+	
+end
+
+
+
+endmodule 
