dfd4fb3a5118803c60b43f4df03f48e74fa05e11
Juan Luis Magaña Paz <opomola@gmail.com>
2021-12-11 19:16:11 -0600

    PLL configuration and module to implementation in DE10-Lite

---
+++ GPIO_Wrapper/src/PLL.v
@@ -0,0 +1,22 @@
+module PLL
+(	input clk_i, 
+	input reset,
+	output clk_o
+);
+
+  localparam SPEED= 1000000; 
+  integer counter = 0;
+   
+  always@ (negedge reset, posedge clk_i) begin
+	   if (reset == 1'b0 ) begin 
+			counter <= 0;
+		end
+	   else begin
+				if (counter == SPEED) counter <=0;
+				else counter <= counter + 1;
+		end		
+	end
+	
+	assign clk_o = counter;
+	
+endmodule 
